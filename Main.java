69f55828b69f0c73a26115c4cfd04fd4f0a8827d
Ayako-Saito <sonaare-urenashi@infoseek.jp>
2019-05-20 17:30:53 +0900

    change

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 class Main{
     public static void main(String[]args){
-        // System.out.println("Hello World");
+        System.out.println("Hello World");
         System.out.println(5+3);
+
+        // データ型　10をnumberに代入
+        int number = 10;
+        System.out.println(number+3);
+        
+        // numberの上書き / データ型を書かない 
+        number = 2;
+        System.out.println(number+3);
     }
 }
 
