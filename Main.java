c101de77d1b6dc7bd40426f98e151072f77d9e95
Ayako-Saito <sonaare-urenashi@infoseek.jp>
2019-07-31 12:13:27 +0900

    フォーマット

--- Main.java
+++ Main.java
@@ -1,19 +1,18 @@
-class Main{
-    public static void main(String[]args){
+class Main {
+    public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println(5+3);
+        System.out.println(5 + 3);
 
-        // データ型　10をnumberに代入
+        // データ型 10をnumberに代入
         int number = 10;
-        System.out.println(number+3);
-        
-        // numberの上書き / データ型を書かない 
+        System.out.println(number + 3);
+
+        // numberの上書き / データ型を書かない
         number = 2;
-        System.out.println(number+3);
+        System.out.println(number + 3);
 
         String name;
         name = "Nyanko";
-        System.out.println("名前は"+name+"です");
+        System.out.println("名前は" + name + "です");
     }
 }
-
